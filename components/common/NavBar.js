e8d367899a32d6db27360587e66c73269de1247c
Conrad Damon <conrad_damon@yahoo.com>
2020-04-04 18:19:36 -0700

    initial big commit

---
+++ components/common/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './NavBar.css';
+
+function NavBar() {
+
+  return (
+    <p className="NavBar">
+      Play | Games | FAQ | Help | Contact
+    </p>
+  );
+}
+
+export default NavBar;
